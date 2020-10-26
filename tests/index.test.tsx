a515158076450a7400db383b92742eb7d981a4b1
Anthony Yarbrough <asyarb@hawaii.edu>
2020-10-25 19:03:21 -1000

    test: currying for dynamic build-time data

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -7,7 +7,7 @@ import { useCachedQuery, QueryCache, CacheProvider } from '../src'
 test('preloading works', async () => {
   let fetchData = async () => 'data'
   let Component = () => {
-    const data = useCachedQuery(fetchData, 'key')
+    let data = useCachedQuery(fetchData, 'key')
 
     return <div>{data}</div>
   }
@@ -22,4 +22,25 @@ test('preloading works', async () => {
   )
 
   expect(getByText('data')).toBeInTheDocument()
+})
+
+test('currying variables works', async () => {
+  let fetchData = (value: string) => async () => value
+  let Component = ({ value }: { value: string }) => {
+    let data = useCachedQuery(fetchData(value), 'currying')
+
+    return <div>{data}</div>
+  }
+
+  let cache = new QueryCache()
+  let ssrValue = 'some-data'
+  await cache.preload(<Component value={ssrValue} />)
+
+  let { getByText } = render(
+    <CacheProvider cache={cache}>
+      <Component value={ssrValue} />
+    </CacheProvider>
+  )
+
+  expect(getByText(ssrValue)).toBeInTheDocument()
 })
